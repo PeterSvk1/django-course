dac37b03682473bd551595e95653b1017e26491f
peter <peter.vlacuha@pobox.sk>
2024-06-29 17:19:18 +0100

    maybe works on vscode

--- my_project/settings.py
+++ my_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-rrhkca-i*5qwh56xmdcu3l2%()_hp81z)d#sc#adf0d%!a99a!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
